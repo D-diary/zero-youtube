2ac2bcb2af8240dbcc204d4982249a15011f67f7
D-diary <dlrlsud@naver.com>
2022-04-20 23:16:27 +0900

    마우스 오버 시에 webp 재생

--- practice/script.js
+++ practice/script.js
@@ -6,11 +6,16 @@
 
   const $search = get('#search')
   const $list = getAll('.contents.list figure')
-  const $searchButton = get('btn_search')
+  const $searchButton = get('.btn_search')
 
   const init = () => {
     $search.addEventListener('keyup', search)
     $searchButton.addEventListener('click', search)
+    for (let index = 0; index < $list.length; index++) {
+      const $target = $list[index].querySelector('picture')
+      $target.addEventListener('mouseover', onMouseOver)
+      $target.addEventListener('mouseout', onMouseOut)
+    }
   }
 
   const search = () => {
@@ -24,6 +29,16 @@
         $list[index].style.display = 'none'
       }
     }
+  }
+
+  const onMouseOver = (e) => {
+    const webpPlay = e.target.parentNode.querySelector('source')
+    webpPlay.setAttribute('srcset', './assets/sample.webp')
+  }
+
+  const onMouseOut = (e) => {
+    const webpPlay = e.target.parentNode.querySelector('source')
+    webpPlay.setAttribute('srcset', './assets/sample.jpg')
   }
 
   init()
